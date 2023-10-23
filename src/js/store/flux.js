880c4be051b1d75788b713e9bf3e9214a1e4226a
Martin Gallo <136949107+MRTN74@users.noreply.github.com>
2023-10-23 00:50:54 +0000

    Changes

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,3 +1,5 @@
+import { useParams } from "react-router";
+
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
@@ -12,17 +14,34 @@ const getState = ({ getStore, getActions, setStore }) => {
 					background: "white",
 					initial: "white"
 				}
-			]
+			],
+			contacts: [],
+			idToDelete: ""
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
+			eliminarContacto: (item) => {
+				
+				console.log(item)
+				const store = getStore();
+				const actions = getActions();
+				const indexMap = getStore().idToDelete
+				console.log(indexMap)
+
+				var requestOptions = {
+					method: 'DELETE'
+				};
+				
+				fetch("https://playground.4geeks.com/apis/fake/contact/" + indexMap, requestOptions)
+					.then(response => response.json())
+					.then( () => {
+						fetch('https://playground.4geeks.com/apis/fake/contact/agenda/agenda_javier')
+						.then((response) => response.json())
+						.then((data) => setStore({contacts: data}))
+					})
+					.catch(error => console.log('error', error));
+					
+				getActions().getContacts();	
 			},
 			changeColor: (index, color) => {
 				//get the store
@@ -36,10 +55,61 @@ const getState = ({ getStore, getActions, setStore }) => {
 				});
 
 				//reset the global store
-				setStore({ demo: demo });
-			}
+				setStore({demo: demo});
+			},
+			agregarContacto: (data) => {
+				
+				const store = getStore();
+
+				const requestOptions = {
+					method: 'POST',
+					headers: { 'Content-Type': 'application/json' },
+					body: JSON.stringify(data)
+				}
+				console.log('Agregar Contacto')
+				fetch('https://playground.4geeks.com/apis/fake/contact', requestOptions)
+				.then( (response) => response.json() )
+				.then( (data) => { getActions().getContacts()} )
+			
+			},
+			getContacts: async () => {
+				const store = getStore();
+				let response = await fetch('https://playground.4geeks.com/apis/fake/contact/agenda/agenda_javier')
+			
+				let data = await response.json()
+
+				if (response.ok){
+				  setStore({
+					contacts: data
+				  })
+				  console.log('existe el usuario')
+				}
+			  
+			},
+			editarContacto: (contact, theid) =>{
+				const store = getStore();
+				const actions = getActions();
+				
+			    const requestOptions = {
+					method: 'PUT',
+					headers: { 'Content-Type': 'application/json' },
+					body: JSON.stringify( contact )
+				};
+				fetch(`https://playground.4geeks.com/apis/fake/contact/${theid}`, requestOptions)
+					.then((response) => response.json())
+					.then((data) => actions.getContacts())
+					.catch((error)=>console.log(error))
+			},
+			saveToDelete: (theid) =>{
+				setStore({
+					idToDelete: theid
+				})
+			} 
+			
 		}
-	};
+			
+	}
 };
+
 
 export default getState;
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,70 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
-import "../../styles/home.css";
+import React, { useState, useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
+import { EditContact } from "./editcontact.js";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPhone } from '@fortawesome/free-solid-svg-icons'
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faPen } from '@fortawesome/free-solid-svg-icons'
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
+import { Context } from "../store/appContext";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+import "../../styles/demo.css";
+
+export const Home = () => {
+	const { store, actions } = useContext(Context);
+	
+	return (
+		<div className="container">
+			<ul className="list-group py-5">
+			<h1 className="py-3">Contact List</h1>
+			{store.contacts.map((item) => 
+				
+					<li
+						className="list-group-item d-flex"
+						>
+						<img src="https://picsum.photos/200" alt=""></img>
+						<div className="d-block px-5">
+						<h1>{item.full_name}</h1>
+						<p className="info"><FontAwesomeIcon icon={faEnvelope} />  {item.email}</p>
+						<p className="info"><FontAwesomeIcon icon={faLocationDot} />{item.address}</p>
+						<p className="info"><FontAwesomeIcon icon={faPhone} />{item.phone}</p>
+						</div>
+						<div className="pencontainer">
+						<Link to={`/editcontact/${item.id}`}>
+							<p className="pen"><FontAwesomeIcon icon={faPen} /></p>
+						</Link>
+						</div>
+						<div className="trashcan px-5">
+							<button onClick= { actions.saveToDelete(item.id) } type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+							<FontAwesomeIcon icon={faTrashCan} />
+							</button>
+
+							<div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+								<div className="modal-dialog">
+									<div className="modal-content">
+									<div className="modal-header">
+										<h1 className="modal-title fs-5" id="exampleModalLabel">Delete contact</h1>
+										<button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+									</div>
+									<div className="modal-body">
+										Are you sure to delete this contact?
+									</div>
+									<div className="modal-footer">
+										<button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+										<button onClick={ () => actions.eliminarContacto(item)} type="button" data-bs-dismiss="modal" className="btn btn-primary">Delete</button>
+									</div>
+									</div>
+								</div>
+							</div>
+						</div>	
+					</li>
+			)}
+			</ul>
+			<br />
+			<Link to="/">
+				<button className="btn btn-primary">Back home</button>
+			</Link>
+		</div>
+	);
+};
